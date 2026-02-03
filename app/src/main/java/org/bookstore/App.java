85ce268bb4e701ae5b2cb978f334d60ef66528a5
Sebastiaan Schneider <Sebastiaanschneider@Gmail.com>
2026-02-03 14:53:04 +0100

    cleanup: removed public from app main

--- app/src/main/java/org/bookstore/App.java
+++ app/src/main/java/org/bookstore/App.java
@@ -8,5 +8,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App
 {
-    public static void main( String[] args ){ SpringApplication.run(App.class, args); }
+    static void main( String[] args ){ SpringApplication.run(App.class, args); }
 }
